5b2916f90bb0c97f633e11e2ff4495e86d450ee2
Allen Wild <allenwild93@gmail.com>
2021-05-26 02:18:01 -0400

    libarchive: add basic example to see if it works
    Yay, it does work! The basic example can list the contents of archives,
    like `bsdtar tf <file>`

---
+++ libarchive/examples/basic.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs::File;
+
+use libarchive::ArchiveReader;
+
+fn main() {
+    let filename = env::args_os().nth(1).unwrap();
+    let file = File::open(&filename).unwrap();
+
+    let mut reader = ArchiveReader::new(file).unwrap();
+
+    loop {
+        match reader.read_next_header() {
+            Ok(Some(entry)) => println!("{}", entry.pathname().unwrap().display()),
+            Ok(None) => break,
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
