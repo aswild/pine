6613c91d9597842b7ff700f30e22369994628b89
Allen Wild <allenwild93@gmail.com>
2021-06-14 00:35:34 -0400

    libarchive: add a build.rs hack to recognize a custom libarchive.so symlink
    Sometimes libarchive is installed but libarchive-dev isn't, and I don't
    have root on the server and don't want to futz with homedir-installing
    it myself (especially true since -dev packages for the various
    libarchive compression libraries might be missing).
    
    With this, if pkg-config can't fine libarchive, then look for
    a libarchive.so symlink in either pine/ or pine/libarchive/ that the
    user would create manually. Link against that libarchive.so as a way of
    pretending that the -dev package is really installed.

--- libarchive/build.rs
+++ libarchive/build.rs
@@ -2,5 +2,46 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 fn main() {
-    pkg_config::Config::new().atleast_version("3.0.0").probe("libarchive").unwrap();
+    eprintln!("Searching for libarchive>=3.0.0 with pkg-config.");
+    if let Ok(_) = pkg_config::Config::new().atleast_version("3.0.0").probe("libarchive") {
+        // probe() printed all the relevant cargo metadata output so we don't have to do anything
+        eprintln!("libarchive found using pkg-config");
+        return;
+    }
+
+    // This hack looks for libarchive.so (probably a symlink) either in the libarchive
+    // directory here, or in the parent pine directory. If found, we use our local directory as
+    // a native library search path and tell rustc to link with it. This is a hack, but it
+    // works here because all we need to do is link against libarchive.so, we don't really need
+    // anything else pkg-config would tell us, and lets pine work on systems that have
+    // libarchive installed but not libarchive-dev. The linker is smart enough to follow the
+    // libarchive.so symlink path to the real library, and no special rpath handling is needed.
+    //
+    // For custom libarchive installations, set PKG_CONFIG_PATH and use pkg-config as usual, not
+    // this method.
+    eprintln!("Failed to find libarchive using pkg-config, looking for local libarchive instead.");
+
+    // look for libarchive.so in the pine/libarchive directory
+    let mydir = std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    if mydir.join("libarchive.so").exists() {
+        eprintln!("Found libarchive.so in {}", mydir.display());
+        println!("cargo:rustc-link-search=native={}", mydir.display());
+        println!("cargo:rustc-link-lib=archive");
+        return;
+    }
+
+    // look for libarchive.so in the top-level pine subdirectory
+    let pinedir = mydir.parent().unwrap();
+    if pinedir.join("libarchive.so").exists() {
+        eprintln!("Found libarchive.so in {}", pinedir.display());
+        println!("cargo:rustc-link-search=native={}", pinedir.display());
+        println!("cargo:rustc-link-lib=archive");
+        return;
+    }
+
+    // none of the above methods worked, print a useful message and bail.
+    eprintln!("No libarchive.so found in the local `pine` or `pine/libarchive` directories.\n\
+               Either install the libarchive-dev and pkg-config packages, or create a `libarchive.so` \
+               symlink in the current directory pointing to a suitable `libarchive.so.13` file.");
+    std::process::exit(1);
 }
